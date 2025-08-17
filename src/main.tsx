fc759f4ad83a66f65fc4025d345fbd78262579d7
Troy <1045132927@qq.com>
2025-08-17 09:42:02 +0800

    refactor(main): 将主逻辑封装为异步函数以提高可读性

--- src/main.tsx
+++ src/main.tsx
@@ -13,16 +13,18 @@ import authController from './controllers/auth-controller.ts'
 import './styles/global.css'
 import './styles/style.css'
 
-if (authController.isAuthenticated) {
-  await authController.checkRedirectUrl()
-}
+;(async function main() {
+  if (authController.isAuthenticated) {
+    await authController.checkRedirectUrl()
+  }
 
-const router = createBrowserRouter(routes)
-console.log(routes)
+  const router = createBrowserRouter(routes)
+  console.log(routes)
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ThemeProvider>
-    <RouterProvider router={router} />
-    <Toaster position="top-right" />
-  </ThemeProvider>
-)
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <ThemeProvider>
+      <RouterProvider router={router} />
+      <Toaster position="top-right" />
+    </ThemeProvider>
+  )
+})()
